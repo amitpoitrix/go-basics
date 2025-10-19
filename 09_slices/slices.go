aff777cbec1ee096422de04ee677392e284576c7
amitpoitrix <amit.poitrix@gmail.com>
2025-10-19 11:44:20 +0530

    Added slices comments

--- 09_slices/slices.go
+++ 09_slices/slices.go
@@ -2,79 +2,78 @@ package main
 
 import (
 	"fmt"
-	"slices"
 )
 
-// slice -> Dynamic Array in Go & most used construct + useful methods
+// Slices -> Dynamic Array in Go. Most used construct & useful methods
+// Slices are reference types
+// Slices are built on top of arrays
+// Slices have dynamic size
+// Slices have length and capacity
+// Length -> no. of elements present in slice
+// Capacity -> maximum no. of elements that can be stored in slice without allocating new underlying array
+// When capacity is exceeded, a new underlying array is allocated with double the capacity
+
 func main() {
-	// uninitialized slice is nil (or Null in other programming language)
+	// Creating a slice using make function
+	slice1 := make([]int, 3, 5) // length 3, capacity 5
+	fmt.Println("slice1:", slice1)
+	fmt.Println("len(slice1):", len(slice1))
+	fmt.Println("cap(slice1):", cap(slice1))
 
-	// var num []int
-	// fmt.Println(num)
-	// fmt.Println(num == nil)
-	// fmt.Println(len(num))
+	// Creating a slice using slice literal
+	slice2 := []int{1, 2, 3, 4, 5}
+	fmt.Println("slice2:", slice2)
+	fmt.Println("len(slice2):", len(slice2))
+	fmt.Println("cap(slice2):", cap(slice2))
 
+	// Slicing an array to create a slice 
+	// Syntax: array[startIndex:endIndex] (endIndex is exclusive)
+	arr := [5]int{10, 20, 30, 40, 50} // array of 5 integers
+	slice3 := arr[1:4] // elements from index 1 to 3
+	fmt.Println("slice3 after slicing an array:", slice3)
+	fmt.Println("len(slice3):", len(slice3))
+	fmt.Println("cap(slice3):", cap(slice3))
 
-	// using make() to create slice
+	// Appending elements to a slice
+	slice4 := []int{1, 2, 3}
+	slice4 = append(slice4, 4, 5)
+	fmt.Println("slice4 after append:", slice4)
 
-	// this will create slice of initial size & capacity as 2 with value as 0
-	// var num = make([]int, 2) // 2nd parameter is initial size, 3rd one is capacity
-	// fmt.Println(num)
-	// fmt.Println(num == nil)
-	// fmt.Println(len(num))
-	// // capacity -> maximum no. of elements that can fit in slice if this size increase
-	// // than it'll dynamically double the capacity
-	// fmt.Println(cap(num))
+	// Copying slices
+	slice5 := make([]int, len(slice4))
+	copy(slice5, slice4)
+	fmt.Println("slice5 after copy:", slice5)
+	slice5[0] = 10
+	fmt.Println("slice5 after modification:", slice5)
+	fmt.Println("slice4 remains unchanged:", slice4)
 
-	// using make() to create slice with capacity
-	// var num = make([]int, 2, 5) // because of intial size 2, 2 zeroed value will be appended
-	// num = append(num, 1) // Adding elements in slice using append()
-	// num = append(num, 2)
-	// num = append(num, 3)
-	// num = append(num, 4)
+	// Multi-dimensional slices
+	slice6 := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+	}
+	fmt.Println("slice6:", slice6)
 
-	// num[0] = 1 // Assigning value at 0th index
+	// Modifying slices
+	slice6[0][0] = 10
+	fmt.Println("slice6 after modification:", slice6)
 
-	// fmt.Println(num)
-	// fmt.Println(len(num))
-	// fmt.Println(cap(num))
+	// Demonstrating that slices are reference types
+	slice7 := slice6
+	slice7[1][1] = 20
+	fmt.Println("slice6 after modifying slice7:", slice6)
+	fmt.Println("slice7:", slice7)
 
+	// Creating a slice from another slice
+	slice8 := slice4[1:4]
+	fmt.Println("slice8:", slice8)
 
-	// Another way of creating slice with 0 initial size & dynamic capacity
-	// nums := []int{}
-	// nums = append(nums, 1)
-	// nums = append(nums, 2)
-	// nums = append(nums, 3)
-	
-	// fmt.Println(nums)
-	// fmt.Println(len(nums))
-	// fmt.Println(cap(nums))
+	// Length and Capacity of slice8
+	fmt.Println("len(slice8):", len(slice8))
+	fmt.Println("cap(slice8):", cap(slice8))	
 
-
-	// copy slice from one to another using copy(destination[], source[])
-	// var nums1 = make([]int, 0, 5)
-	// nums1 = append(nums1, 2)
-	// var nums2 = make([]int, len(nums1))
-
-	// copy(nums2, nums1)
-
-	// fmt.Println(nums1, nums2)
-
-
-	// slice Operator arr[:]
-	// var nums = []int{1,2,3,4,5}
-	// fmt.Println(nums[0:2])
-	// fmt.Println(nums[:2])
-	// fmt.Println(nums[3:])
-
-
-	// comparing 2 slice using slices pkg Equal()
-	var num1 = []int{1,2,1}
-	var num2 = []int{1,2,1}
-	fmt.Println(slices.Equal(num1, num2))
-
-
-	// 2-D slices
-	var nums = [][]int{{1,2,3}, {4,5}}
-	fmt.Println(nums)
+	// Appending to slice8 and observing changes in slice4
+	slice8 = append(slice8, 6, 7)
+	fmt.Println("slice8 after append:", slice8)
+	fmt.Println("slice4 after slice8 append:", slice4)
 }
